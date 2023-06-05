7e3b31178b0e0f1048bbc9b039de07c24600d6fe
MheadHero <zhenxian0@gmail.com>
2023-06-05 21:50:58 +0800

    day 7 php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+//day 7 connect to mysql using pdo object
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=project2notecrud;user=root;password=Mhead1864,;charset=utf8mb4';
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('SELECT * FROM posts');
+$statement->execute();
+
+$notes = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($notes as $note){
+    echo "<li>" . $note['title'] . "</li>";
+}
+
 
