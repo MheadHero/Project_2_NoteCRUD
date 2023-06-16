aac83c765d3a4fe07ad0908b3f86ab083da4e07a
MheadHero <zhenxian0@gmail.com>
2023-06-16 20:54:40 +0800

    day 16 php

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,17 @@
 <?php 
+$config = require 'config.php';
+
+//$db = new Database($config['database'], 'root', 'Mhead1864,');
+$db = new Database($config['database']);
 
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO `project2notecrud`.`notes` (`body`, `userid`) VALUES (:body, :userid)', [
+        'body' => $_POST['body'],
+        'userid' => 1
+    ]);
 }
+
 
 require 'views/note-create.view.php';
--- views/note.view.php
+++ views/note.view.php
@@ -5,7 +5,7 @@
     <main>
       <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p class= 'mb-6'>
-          <?= $note['body']; ?>
+          <?= htmlspecialchars($note['body']); ?>
         </p>
 
         <p>
