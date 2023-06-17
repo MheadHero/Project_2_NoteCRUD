999775e775ae65f93bd53755817c416763065d4a
MheadHero <zhenxian0@gmail.com>
2023-06-17 20:58:04 +0800

    php day 17

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,6 +7,17 @@ $db = new Database($config['database']);
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $errors = [];
+
+    if (empty($_POST['body'])) {
+        $errors['body'] = 'Body is required';
+    }
+
+    if (strlen($_POST['body']) > 500) {
+        $errors['body'] = 'Body must be less than 500 characters';
+    }
+
     $db->query('INSERT INTO `project2notecrud`.`notes` (`body`, `userid`) VALUES (:body, :userid)', [
         'body' => $_POST['body'],
         'userid' => 1
